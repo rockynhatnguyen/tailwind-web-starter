14e42c0190f1ee0d9fab415100cc85c123f80c47
Rocky Nguyen <rocky.nguyen@digitaloperative.com>
2020-12-07 17:48:18 -0800

    build: tailwind header
    Added a header with clickable menu and hoverable menu. Needs proper styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   theme: {
+    container: {
+      center: true,
+      padding: '8rem',
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['Roboto', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
     extend: {
       colors: {
-        'ws-blue': {
+        'roccat-blue': {
           100: '#E6F0FF',
           200: '#BFDAFF',
           300: '#99C3FF',
@@ -13,6 +27,17 @@ module.exports = {
           800: '#003072',
           900: '#00204C',
         },
+        'h1-light': {
+          100: '#F1F1F1',
+          200: '#F2F9E9',
+          300: '#F3F3F9',
+          400: '#F4F4F9',
+          500: '#F5F5F9',
+          600: '#F6F6F9',
+          700: '#F7F7F9',
+          800: '#F8F8F9',
+          900: '#F9F9F9',
+        }
       }
     },
   },
